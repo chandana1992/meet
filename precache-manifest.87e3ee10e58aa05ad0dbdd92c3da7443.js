16704f4676ae3388610999d56fa60ceec1d33c5a
chandana1992 <chandana@careerfoundy.com>
2020-06-25 18:05:22 +0200

    Updates

---
+++ precache-manifest.87e3ee10e58aa05ad0dbdd92c3da7443.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b0eae0c9b7c55a1781a175751efae4e",
+    "url": "/meet/index.html"
+  },
+  {
+    "revision": "315d72a521b24401bc1d",
+    "url": "/meet/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "bad5777b1a072d8a8c74",
+    "url": "/meet/static/js/2.d2ee30b2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/meet/static/js/2.d2ee30b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "315d72a521b24401bc1d",
+    "url": "/meet/static/js/main.d3086dd5.chunk.js"
+  },
+  {
+    "revision": "46b22fff6326d574d8f4",
+    "url": "/meet/static/js/runtime-main.c7790544.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/meet/static/media/logo.5d5d9eef.svg"
+  }
+]);
